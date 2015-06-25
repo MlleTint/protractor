dbe8773b3adf65118d43fc8feff4b82a9bc2c2b5
Chantel Storstad <stors014@umn.edu>
2015-06-24 18:24:12 -0600

    protractor test

---
+++ spec.js
@@ -0,0 +1,8 @@
+// spec.js
+describe('Protractor Nav', function() {
+  it('there is a nav bar', function() {
+  it('there are 3 nav options', function() {
+  it('the last option is named messages', function() {
+    
+  });
+});
